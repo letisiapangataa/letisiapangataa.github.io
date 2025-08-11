b396795856b7fd1634e3b942b639215901abc1a8
Letisia Pangata'a <51559861+letisiapangataa@users.noreply.github.com>
2025-08-11 15:18:55 +1200

    New added files.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'production';
-
-const repo = 'letisiapangataa.github.io'; // your repo name
-
 module.exports = {
   output: 'export',
-  assetPrefix: isGithubPages ? `/${repo}/` : '',
   images: { unoptimized: true },
   trailingSlash: true,
-  basePath: isGithubPages ? `/${repo}` : '',
 };
